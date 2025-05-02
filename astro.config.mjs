944618fb9016c4e961068f150365d905fa4ed9bf
Meet Joshi <meetjoshiraru@gmail.com>
2025-05-02 10:13:33 +0000

    fix: add missing comma in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meetjoshi.info',
-  base: 'meetjoshi.info'
+  base: 'meetjoshi.info',
   prefetch: true,
   integrations: [],
   markdown: {
