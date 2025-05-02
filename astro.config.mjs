855fa058416f76402ba5979e743d70914e0264b8
Meet Joshi <meetjoshiraru@gmail.com>
2025-05-02 20:25:01 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meetjoshi.info',
-  base: 'meetjoshi.info',
   prefetch: true,
   integrations: [],
   markdown: {
